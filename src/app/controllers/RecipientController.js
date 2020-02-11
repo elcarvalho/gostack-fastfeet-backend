b6e102b8d5dda6af2558b7eca730d541d247c816
elcarvalho <lessa.eric@gmail.com>
2020-02-11 20:10:02 -0200

    update: implementando regras para criação/atualização de destinatários

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,10 +1,80 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
 class RecipientController {
-  store(req, res) {
-    res.json({ message: 'store' });
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { name, street, number, complement, state, city, zip } = req.body;
+
+    const recipient = await Recipient.create({
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    });
+
+    return res.json(recipient);
   }
 
-  update(req, res) {
-    res.json({ message: 'update' });
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not exists' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    });
   }
 }
 
