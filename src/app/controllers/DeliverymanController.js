961acfcf1b502c558588f22d0ec346d37df91a6d
elcarvalho <lessa.eric@gmail.com>
2020-02-27 17:30:07 -0300

    chore(gestão de entregadores): implementando store

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,10 +1,27 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+
 class DeliveryController {
   async index(req, res) {
     return res.json({ ok: true });
   }
 
   async store(req, res) {
-    return res.json({ ok: true });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      avatar_id: Yup.number(),
+      email: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { name, avatar_id, email } = req.body;
+
+    const deliveryman = await Deliveryman.create({ name, avatar_id, email });
+
+    return res.json(deliveryman);
   }
 
   async update(req, res) {
