ea43d8507c78a7e7d50075b61981fbc8ea010c4b
elcarvalho <lessa.eric@gmail.com>
2020-02-28 17:04:44 -0300

    chore(gestão de encomendas): cadastro de encomendas

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Order from '../models/Order';
 
 class OrderController {
@@ -12,7 +13,29 @@ class OrderController {
   }
 
   async store(req, res) {
-    return res.json({ ok: true });
+    const schema = Yup.object().shape({
+      recipientId: Yup.number().required(),
+      deliverymanId: Yup.number().required(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { recipientId } = req.body;
+
+    const order = await Order.findOne({ where: { recipientId } });
+
+    if (order) {
+      return res.status(400).json({ error: 'The order was assigned already.' });
+    }
+
+    const { deliverymanId, product } = await Order.create(req.body);
+
+    // TODO: notificar entregador por e-mail
+
+    return res.json({ recipientId, deliverymanId, product });
   }
 
   async update(req, res) {
