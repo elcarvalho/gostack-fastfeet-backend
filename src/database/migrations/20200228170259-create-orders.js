cebd1a231e7a609b7359c749c526291627649e4f
elcarvalho <lessa.eric@gmail.com>
2020-02-28 15:14:14 -0300

    chore(gestão de encomendas): migration tabela de encomendas

---
+++ src/database/migrations/20200228170259-create-orders.js
@@ -0,0 +1,52 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('orders');
+  },
+};
