7673ec1b44bdb1c706edf7e5fe0918eb340fb522
elcarvalho <lessa.eric@gmail.com>
2020-03-02 17:58:01 -0300

    chore(funcionalidades do entregador): migration/model delivery problems

---
+++ src/app/models/DeliveryProblem.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize';
+
+class DeliveryProblem extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        order_id: Sequelize.INTEGER,
+        description: Sequelize.STRING,
+      },
+      { sequelize }
+    );
+
+    return this;
+  }
+}
+
+export default DeliveryProblem;
--- src/database/index.js
+++ src/database/index.js
@@ -5,10 +5,11 @@ import Recipient from '../app/models/Recipient';
 import Deliveryman from '../app/models/Deliveryman';
 import Order from '../app/models/Order';
 import File from '../app/models/File';
+import DeliveryProblem from '../app/models/DeliveryProblem';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, Deliveryman, Order, File];
+const models = [User, Recipient, Deliveryman, Order, File, DeliveryProblem];
 
 class Database {
   constructor() {
